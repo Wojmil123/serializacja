2aba3d16a40fd8a2e8a57599a98345c31120a598
Jerzy Kisielewski <wojmilgamon@gmail.com>
2023-04-29 19:06:28 +0200

    create project create Player

---
+++ src/Player.java
@@ -0,0 +1,30 @@
+import java.io.Serial;
+import java.io.Serializable;
+public class Player implements Serializable{
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private String leftArm;
+    private String rightArm;
+    private String hp;
+    private String level;
+
+    Player(String name, String hp, String level) {
+        this.name = name;
+        this.hp = hp;
+        this.level = level;
+    }
+
+    public void useLeftArm(String item){
+        this.leftArm = item;
+    }
+
+    public void useRightArm(String item){
+        this.rightArm = item;
+    }
+
+    @Override
+    public String toString(){
+        return "name: " + this.name + "\r\nhp: " + this.hp + "\r\nlevel: " + this.level + "\r\nright arm: " + this.rightArm + "\r\nleft arm: " + this.leftArm;
+    }
+}
